0daeb40b044457b6e1d830b467def7b4f54069c4
Hunter Smith <hunter101167@yahoo.com>
2024-05-16 19:31:35 -0700

    Assignment 2

---
+++ W2A2Q1/BankAccountGUI.java
@@ -0,0 +1,74 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BankAccountGUI extends JFrame {
+    private JTextField initialDepositField;
+    private JTextField interestRateField;
+    private JTextField yearsField;
+    private JButton calculateButton;
+    private JLabel resultLabel;
+
+    public BankAccountGUI() {
+        setLayout(new GridLayout(5, 2));
+
+        add(new JLabel("Initial Deposit:"));
+        initialDepositField = new JTextField();
+        add(initialDepositField);
+
+        add(new JLabel("Annual Interest Rate:"));
+        interestRateField = new JTextField();
+        add(interestRateField);
+
+        add(new JLabel("Number of Years:"));
+        yearsField = new JTextField();
+        add(yearsField);
+
+        calculateButton = new JButton("Calculate");
+        add(calculateButton);
+
+        resultLabel = new JLabel("Final Balance: ");
+        add(resultLabel);
+
+        calculateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double initialDeposit = Double.parseDouble(initialDepositField.getText());
+                double annualInterestRate = Double.parseDouble(interestRateField.getText());
+                int years = Integer.parseInt(yearsField.getText());
+
+                BankAccount account = new BankAccount(initialDeposit, annualInterestRate);
+                double finalBalance = account.calculateFinalBalance(years);
+
+                resultLabel.setText("Final Balance: " + finalBalance);
+            }
+        });
+
+        setSize(300, 150);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new BankAccountGUI();
+    }
+}
+
+class BankAccount {
+    private double balance;
+    private double annualInterestRate;
+
+    public BankAccount(double initialDeposit, double annualInterestRate) {
+        this.balance = initialDeposit;
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    //I think this is how compounded intrest is gotten?
+    public double calculateFinalBalance(int years) {
+        for (int i = 0; i < years; i++) {
+            balance += balance * annualInterestRate;
+        }
+        return balance;
+    }
+}
--- W2A2Q2/clock.java
+++ W2A2Q2/clock.java
@@ -1,8 +1,31 @@
-import java.util.*;
+import java.awt.event.*;
+import java.util.Date;
+import javax.swing.*;
 
-public class clock {
-    public static void main(String[] args) {
+class TimerHandler implements ActionListener {
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(new Date());
+	}
+}
 
+public class clock implements ActionListener {
+	
+	public clock() {
+		Timer t = new Timer(1000, new TimerHandler());
+		t.start();
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new clock();
+			}
+		});
+	}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
-
 }
